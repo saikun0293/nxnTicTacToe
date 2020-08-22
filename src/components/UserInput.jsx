d703808d0e05798a08b59f8d273cfeaadf97e6b9
SaitejareddyTileti-0293 <saitejareddytileti@gmail.com>
2020-08-22 19:16:39 +0530

    change

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import apiKey from "./APIKEY";
 import Axios from "axios";
-import "../styles/UserInput.css";
-import { FontAwesomeIcon } from "../../node_modules/@fortawesome/react-fontawesome";
-import { faSlack } from "../../node_modules/@fortawesome/free-brands-svg-icons";
-import { faJoomla } from "../../node_modules/@fortawesome/free-brands-svg-icons";
 
 const api = Axios.create({
   baseURL: "https://crudcrud.com/api/" + apiKey,
@@ -44,50 +40,19 @@ class UserInput extends Component {
 
   render() {
     return (
-      <div>
-        <div className="flex-row1">
-          <FontAwesomeIcon className="fajoomla" icon={faJoomla} color="blue" />
-          <FontAwesomeIcon className="faslack" icon={faSlack} color="red" />
-        </div>
-        <div className="flex-row">
-          <div className="userinput-field">
-            <form className="blueteam-ui" onSubmit={this.handleSubmit}>
-              <label htmlFor="username">Username</label>
-              <input
-                name="username"
-                type="text"
-                onChange={(e) => this.handleInput(e)}
-              />
-              <label htmlFor="password">Password</label>
-              <input
-                name="password"
-                type="password"
-                onChange={(e) => this.handleInput(e)}
-              />
-              <button className="verify-btn" type="submit">
-                Verify
-              </button>
-            </form>
-            <form className="redteam-ui" onSubmit={this.handleSubmit}>
-              <label htmlFor="username">Username</label>
-              <input
-                name="username"
-                type="text"
-                onChange={(e) => this.handleInput(e)}
-              />
-              <label htmlFor="password">Password</label>
-              <input
-                name="password"
-                type="password"
-                onChange={(e) => this.handleInput(e)}
-              />
-              <button className="verify-btn" type="submit">
-                Verify
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <form className="userinput-field" onSubmit={this.handleSubmit}>
+        <input
+          name="username"
+          type="text"
+          onChange={(e) => this.handleInput(e)}
+        />
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => this.handleInput(e)}
+        />
+        <button type="submit">Verify</button>
+      </form>
     );
   }
 }
